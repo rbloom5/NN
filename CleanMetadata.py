4fd7735b4dcdcdf2e5ab7984d4fa5af094b48adf
ramanktalwar <ramancoke.3@gmail.com>
2015-06-25 17:42:03 -0700

    Updated Moderate responders in CleanMetadata
    A couple of changes…
    
    1. the function is now called tnfa_response() so that I can add other
    data sets in that file for you
    
    2. The function now take an argument called Moderates which by default
    is set to False. If Moderates is false it will not include any
    moderates. If it is set to 1, it will include them and group them with
    the responders. If it is set to 2, it will create a new group where
    medium response = 2 (i know it’s non intuitive but it was a lot easier
    that way.)

--- CleanMetadata.py
+++ CleanMetadata.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-def slice_and_clean():
+def tnfa_response(Moderates=False):
 
 	full_DF = pd.read_csv('tnfa_response_expression.txt')
 	full_metadata_DF = pd.read_csv('tnfa_response_metadata.txt')
@@ -17,13 +17,15 @@ def slice_and_clean():
 			if full_metadata_DF[sample].loc['characteristics_ch1.4'].split(': ')[1] == 'Placebo':
 				drop_list.append(sample)
 			#Remove the moderate responders from GSE58795
-			if full_metadata_DF[sample].loc['characteristics_ch1.5'].split(': ')[1] == 'moderate':
-				drop_list.append(sample)
+			if Moderates == False:
+				if full_metadata_DF[sample].loc['characteristics_ch1.5'].split(': ')[1] == 'moderate':
+					drop_list.append(sample)
 
 		if sample.split('._.')[0] == 'GSE15258':
 			#Remove the MEDIUM responders
-			if full_metadata_DF[sample].loc['characteristics_ch1.5'].split(': ')[1] == 'MEDIUM':
-				drop_list.append(sample)
+			if Moderates == False:
+				if full_metadata_DF[sample].loc['characteristics_ch1.5'].split(': ')[1] == 'MEDIUM':
+					drop_list.append(sample)
 			#Remove the NA responders
 			if full_metadata_DF[sample].loc['characteristics_ch1.5'].split(': ')[1] == 'NA':
 				drop_list.append(sample)
@@ -50,12 +52,18 @@ def slice_and_clean():
 				response = 0
 			if subset_metadata_DF[sample].loc['characteristics_ch1.5'].split(': ')[1] == 'responder':
 				response = 1
+			if subset_metadata_DF[sample].loc['characteristics_ch1.5'].split(': ')[1] == 'moderate':
+				if Moderates == 1: response = 1
+				if Moderates == 2: response = 2
 
 		if sample.split('._.')[0] == 'GSE15258':
 			if subset_metadata_DF[sample].loc['characteristics_ch1.5'].split(': ')[1] == 'NORESPONSE':
 				response = 0
 			if subset_metadata_DF[sample].loc['characteristics_ch1.5'].split(': ')[1] == 'RESPONSE':
 				response = 1
+			if subset_metadata_DF[sample].loc['characteristics_ch1.5'].split(': ')[1] == 'MEDIUM':
+				if Moderates == 1: response = 1
+				if Moderates == 2: response = 2
 
 		if sample.split('._.')[0] == 'GSE33377':
 			if subset_metadata_DF[sample].loc['characteristics_ch1.2'].split(' ')[2] == 'non-responder':
@@ -71,7 +79,7 @@ def slice_and_clean():
 
 		response_list.append(response)
 
-	return subset_DF, response_list
+	return subset_DF, subset_metadata_DF, response_list
 
 
 
